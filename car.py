d5417533cc86c669aa0e307ad93b501e0b5bada7
Exodus <kubagr2000@gmail.com>
2023-04-02 15:41:12 +0200

    Initial commit

---
+++ car.py
@@ -0,0 +1,41 @@
+import time
+import threading
+import random
+
+class Car:
+    def __init__(self, canvas, direction):
+        self.canvas = canvas
+        self.direction = direction
+
+        random_color = self.get_random_color()
+
+        self.car = canvas.create_rectangle(direction.x1, direction.y1,  direction.x2, direction.y2, fill = random_color)
+
+        self.move_thread = None
+        self.x_speed = direction.x_speed
+        self.y_speed = direction.y_speed
+
+    def start(self):
+        self.move_thread = threading.Thread(target = self.move)
+        self.move_thread.start()
+
+    def move(self):
+        while True:
+            x1, y1, x2, y2 = self.canvas.coords(self.car)
+
+            if x1 <= 0 or y1 <= 0:
+                self.canvas.delete(self.car)
+                break
+            elif x2 >= self.canvas.winfo_width() or y2 >= self.canvas.winfo_height():    
+                self.canvas.delete(self.car)
+                break
+            self.canvas.move(self.car, self.x_speed, self.y_speed)
+            time.sleep(0.01)
+
+
+    def get_random_color(self):
+        de=("%02x" % random.randint(0,255))
+        re=("%02x" % random.randint(0,255))
+        we=("%02x" % random.randint(0,255))
+        ge="#"
+        return ge + de + re + we
---
+++ direction.py
@@ -0,0 +1,8 @@
+class Direction:
+    def __init__(self, x1, x2, y1, y2, x_speed, y_speed):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.x_speed = x_speed
+        self.y_speed = y_speed
